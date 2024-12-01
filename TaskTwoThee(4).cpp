c2454c3d15f69081a16497a21070bb7554d2d52b
Van1622 <sevruk.ivan1622@vu.cdu.edu.ua>
2024-12-01 17:58:05 +0200

    Update TaskTwoThee(4).cpp

--- TaskTwoThee(4).cpp
+++ TaskTwoThee(4).cpp
@@ -1,52 +1,55 @@
 
 #include <iostream>
 #include <string>
+#include <limits> 
 
 int main() {
-    std::string firstName1, lastName1;
-    int age1;
-    bool isStudent1;
-    char studentInput1;
+    int userCount = 0;
+    std::cout << "Enter the number of users to input: ";
+    std::cin >> userCount;
 
-    std::cout << "Enter information for the first user:\n";
-    std::cout << "Write your firstname: ";
-    std::cin >> firstName1;
-    std::cout << "Write your lastname: ";
-    std::cin >> lastName1;
-    std::cout << "Write your age: ";
-    std::cin >> age1;
-    std::cout << "Are you a student? (enter 'y' for yes or 'n' for no): ";
-    std::cin >> studentInput1;
-    isStudent1 = (studentInput1 == 'y' || studentInput1 == 'Y');
+    if (userCount <= 0) {
+        std::cout << "Invalid number of users!" << std::endl;
+        return 1;
+    }
 
-    std::string firstName2, lastName2;
-    int age2;
-    bool isStudent2;
-    char studentInput2;
+    for (int i = 0; i < userCount; ++i) {
+        std::string firstName, lastName;
+        int age;
+        bool isStudent;
+        char studentInput;
 
-    std::cout << "\nEnter information for the second user:\n";
-    std::cout << "Write your firstname: ";
-    std::cin >> firstName2;
-    std::cout << "Write your lastname: ";
-    std::cin >> lastName2;
-    std::cout << "Write your age: ";
-    std::cin >> age2;
-    std::cout << "Are you a student? (enter 'y' for yes or 'n' for no): ";
-    std::cin >> studentInput2;
-    isStudent2 = (studentInput2 == 'y' || studentInput2 == 'Y');
+        std::cout << "\nEnter information for user " << i + 1 << ":\n";
 
-    std::cout << "\nInformation of the first user:\n";
-    std::cout << "Firstname: " << firstName1
-              << "\nLastname: " << lastName1
-              << "\nAge: " << age1
-              << "\nAre you a student? " << (isStudent1 ? "yes" : "no") << std::endl;
+        std::cout << "Write your firstname: ";
+        std::cin >> firstName;
 
-    std::cout << "\nInformation of the second user:\n";
-    std::cout << "Firstname: " << firstName2
-              << "\nLastname: " << lastName2
-              << "\nAge: " << age2
-              << "\nAre you a student? " << (isStudent2 ? "yes" : "no") << std::endl;
+        std::cout << "Write your lastname: ";
+        std::cin >> lastName;
+
+        while (true) {
+            std::cout << "Write your age: ";
+            std::cin >> age;
+
+            if (std::cin.fail() || age <= 0) {
+                std::cin.clear(); 
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+                std::cout << "Invalid input. Please enter a positive integer for age.\n";
+            } else {
+                break; 
+            }
+        }
+
+        std::cout << "Are you a student? (enter 'y' for yes or 'n' for no): ";
+        std::cin >> studentInput;
+        isStudent = (studentInput == 'y' || studentInput == 'Y');
+
+        std::cout << "\nInformation of user " << i + 1 << ":\n";
+        std::cout << "Firstname: " << firstName
+                  << "\nLastname: " << lastName
+                  << "\nAge: " << age
+                  << "\nAre you a student? " << (isStudent ? "yes" : "no") << std::endl;
+    }
 
     return 0;
 }
-
