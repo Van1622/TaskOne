dc19556c90e8385910bc635c9133a01f61c980fd
Van1622 <sevruk.ivan1622@vu.cdu.edu.ua>
2024-12-21 12:48:20 +0200

    Update mathlib.h

--- mathlib.h
+++ mathlib.h
@@ -1,1 +1,14 @@
+#ifndef MATHLIB_H
+#define MATHLIB_H
+
+namespace mathlib {
+
+// Оголошення функцій
+double sqrt(double x);       // Квадратний корінь
+double pow(double base, double exponent); // Піднесення до степеня
+double round(double x);      // Округлення
+
+} // namespace mathlib
+
+#endif // MATHLIB_H
 
