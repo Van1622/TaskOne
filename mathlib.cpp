005857006df4a4ef7e5a2aa6f585bfe5fc546b8c
Van1622 <sevruk.ivan1622@vu.cdu.edu.ua>
2024-12-21 12:48:32 +0200

    Update mathlib.cpp

--- mathlib.cpp
+++ mathlib.cpp
@@ -1,1 +1,19 @@
+#include "mathlib.h"
+#include <cmath> // Використовуємо стандартні функції з цієї бібліотеки
+
+namespace mathlib {
+
+double sqrt(double x) {
+    return std::sqrt(x);
+}
+
+double pow(double base, double exponent) {
+    return std::pow(base, exponent);
+}
+
+double round(double x) {
+    return std::round(x);
+}
+
+} // namespace mathlib
 
