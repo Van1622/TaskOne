e23d9a47c30d876ba7e6635c65bf0884d79ba4d7
Van1622 <sevruk.ivan1622@vu.cdu.edu.ua>
2024-12-21 12:48:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "mathlib.h"
+
+int main() {
+    using namespace mathlib;
+
+    double num = 25.0;
+    double base = 2.0, exponent = 3.0;
+
+    std::cout << "sqrt(" << num << ") = " << sqrt(num) << "\n";
+    std::cout << "pow(" << base << ", " << exponent << ") = " << pow(base, exponent) << "\n";
+    std::cout << "round(3.14) = " << round(3.14) << "\n";
+
+    return 0;
+}
 
